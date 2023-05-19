d9c11921a8f2a7a866412d8856b564d8a952575c
Kutay <kutaybekleric@gmail.com>
2023-05-19 13:40:57 +0300

    finish cart logic

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 export default function Cart() {
     const cartRef = useRef();
-    const {totalPrice, totalQuantities, cartItems, setShowCart} = useStateContext();
+    const {totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove} = useStateContext();
 
     return (
         <div className="cart-wrapper" ref={cartRef}>
@@ -23,39 +23,50 @@ export default function Cart() {
                     <div className="empty-cart">
                         <Image src="/icons/shopping-bag.png" alt="Empty Cart" width={150} height={100}/>
                         <h3>Your shopping bag is empty</h3>
-                        <button type="button" onClick={() => setShowCart(false)}>
+
+                        <button type="button" onClick={() => setShowCart(false)} className="" >
                             Continue Shopping
                         </button>
                     </div>
                 )}
                 <div>
-                    {cartItems.length >= 1 && cartItems.map((item) => (
-                        <div className="relative border border-black m-8 w-[80%]" key={item.id}>
+                    {cartItems.length >= 1 && cartItems.map((product) => (
+                        <div className="relative border border-black m-8 w-[80%]" key={product.id}>
                             <div className="flex mt-5 mb-5 w-[100%] ">
                                 <div className="w-[25%]">
-                                    <Image className="ml-5" src={item.image} alt={item.name} width={100}
+                                    <Image className="ml-5" src={product.image} alt={product.name} width={100}
                                            height={100}/>
                                 </div>
                                 <div className="flex ml-12 w-[80%]">
-                                    <h3 className="font-bold w-[60%]">{item.title}</h3>
-                                    <h4 className="ml-10 font-extrabold text-red-600">$ {item.price}</h4>
+                                    <h3 className="font-bold w-[60%]">{product.title}</h3>
+                                    <h4 className="ml-10 font-extrabold text-red-600">$ {product.price}</h4>
                                 </div>
                             </div>
                             <div className="flex justify-between items-center w-[30%] absolute bottom-2 left-36 ">
-                                <div className="w-6 h-6 cursor-pointer  relative rounded-full border border-black"><span
-                                    className="text-3xl absolute -top-2 left-0.5">+</span></div>
-                                <span className="text-3xl">0</span>
-                                <div className="w-6 h-6 cursor-pointer relative rounded-full border border-black"><span
+                                <div onClick={() => toggleCartItemQuantity(product.id, "dec")}
+                                     className="w-6 h-6 cursor-pointer  relative rounded-full border border-black"><span
                                     className="text-4xl absolute -top-3 left-[5px]">-</span></div>
-                                <div className="absolute -right-32">
-                                <Image  className=" " src="/icons/remove.png" alt="Remove Icon" width={20} height={20} />
+                                <span className="text-3xl">{product.quantity}</span>
+                                <div onClick={() => toggleCartItemQuantity(product.id, "inc")}
+                                     className="w-6 h-6 cursor-pointer relative rounded-full border border-black"><span
+                                    className="text-3xl absolute  -top-2 left-0.5">+</span></div>
+                                <div onClick={() => onRemove(item)} className="cursor-pointer absolute -right-32">
+                                    <Image className=" " src="/icons/remove.png" alt="Remove Icon" width={20}
+                                           height={20}/>
                                 </div>
                             </div>
                         </div>
                     ))
                     }
-
                 </div>
+                {cartItems.length >= 1 && (
+                    <div className="absolute w-full p-10 bottom-0">
+                        <div className="flex justify-between text-xl ">
+                            <h3>Subtotal:</h3>
+                            <h3>${totalPrice.toFixed(2)}</h3>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     )
--- src/context/StateContext.tsx
+++ src/context/StateContext.tsx
@@ -7,14 +7,17 @@ const Context = createContext()
 export const StateContext = ({children}: any) => {
     const [showCart, setShowCart] = useState(false)
     const [cartItems, setCartItems] = useState([])
-    const [totalPrice, setTotalPrice] = useState()
+    const [totalPrice, setTotalPrice] = useState(0)
     const [totalQuantities, setTotalQuantities] = useState(0)
     const [qty, setQty] = useState(1)
+
+    let foundProduct
 
     const onAdd = (product, quantity) => {
         const checkProductInCart = cartItems.find((item) => item.id === product.id)
         setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
         setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
+        setQty(1)
 
         if (checkProductInCart) {
             const updatedCartItems = cartItems.map((item) => {
@@ -27,10 +30,39 @@ export const StateContext = ({children}: any) => {
             product.quantity = quantity
             setCartItems([...cartItems, {...product}])
         }
-
         toast.success(`${qty} ${product.title} added to the cart.`)
     }
 
+
+    const onRemove = (product) => {
+        foundProduct = cartItems.find((item) => item.id === product.id);
+        const newCartItems = cartItems.filter((item) => item.id !== product.id);
+
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity);
+        setTotalQuantities(prevTotalQuantities => prevTotalQuantities - foundProduct.quantity);
+        setCartItems(newCartItems);
+    }
+
+    const toggleCartItemQuantity = (id, value) => {
+        foundProduct = cartItems.find((item) => item.id === id)
+
+        if (value === 'inc') {
+            const updatedData = cartItems.map(item => (item.id === id ? {...item, quantity: item.quantity + 1} : item));
+            setCartItems(updatedData);
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+            setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+        } else if (value === 'dec') {
+            if (foundProduct.quantity > 1) {
+                const updatedData = cartItems.map(item => (item.id === id ? {
+                    ...item,
+                    quantity: item.quantity - 1
+                } : item));
+                setCartItems(updatedData);
+                setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+                setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+            }
+        }
+    }
     const incQty = () => {
         setQty((prev) => prev + 1)
     }
@@ -52,7 +84,8 @@ export const StateContext = ({children}: any) => {
             decQty,
             onAdd,
             setShowCart,
-
+            toggleCartItemQuantity,
+            onRemove
         }}>
             {children}
 
