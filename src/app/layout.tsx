62d275571f8bc0b11de440047957a27eda3f5210
Kutay <kutaybekleric@gmail.com>
2023-05-21 19:56:24 +0300

    fix rootlayout useclient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-'use client'
 import Nav from '../components/Nav'
 import { StateContext } from '../context/StateContext'
 import { Toaster } from 'react-hot-toast'
@@ -9,17 +8,17 @@ export const metadata = {
 	description: 'Make a statement with your wardrobe.',
 	icons: {
 		icon: {
-			url: '/favicon.png',
-			type: 'image/png',
+			url: '/favicon.ico',
+			type: 'icon',
 		},
-		shortcut: { url: '/icons/favicon.ico', type: 'image/png' },
+		shortcut: { url: '/icons/favicon.ico', type: 'icon' },
 	},
 }
 export default function RootLayout(props: any) {
 	return (
 		<html lang="en" className="bg-bg-primary">
 			<body>
-				<div className="max-w-[82%] mx-auto " id="div">
+				<div className="mx-auto max-w-[82%] " id="div">
 					<Providers>
 						<StateContext>
 							<Toaster />
