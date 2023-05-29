7f42eb45138dd4c88a4fec9058b046f5041296ea
Kutay <kutaybekleric@gmail.com>
2023-05-29 21:24:35 +0300

    about and contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,10 @@ import "../globals.css"
 
 const Contact = () => {
     return <>
-        <div>contact</div>
+        <div className="mt-10 ml-10">
+            <h1>Contact Page</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam distinctio eaque enim et excepturi magnam maiores natus necessitatibus nostrum placeat quam quisquam ratione rem reprehenderit sequi sunt temporibus, voluptas?</p>
+        </div>
     </>
 
 
